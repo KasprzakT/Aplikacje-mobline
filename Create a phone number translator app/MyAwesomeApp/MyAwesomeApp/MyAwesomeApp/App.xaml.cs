a95b03fac412ef5adfc2caf8a4315539d97b6ebe
KasprzakT <48860607+KasprzakT@users.noreply.github.com>
2021-11-02 12:23:45 +0100

    Upload

---
+++ Create a phone number translator app/MyAwesomeApp/MyAwesomeApp/MyAwesomeApp/App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MyAwesomeApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            MainPage = new Page1();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
